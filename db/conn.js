b56927985bb11ffe8cc435f4ee985fc547d0a938
vitordsrodrigues <165427238+rodriguessilvavitor@users.noreply.github.com>
2024-11-29 21:59:23 -0300

    a

--- db/conn.js
+++ db/conn.js
@@ -1,16 +1,17 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('eventos2','root','',{
-
-    host:'localhost',
-    dialect:'mysql',
+// Ajuste o código com as credenciais fornecidas
+const sequelize = new Sequelize('sql10748608', 'sql10748608', '7Us75d96Qr', {
+    host: 'sql10.freesqldatabase.com',
+    dialect: 'mysql',
+    port: 3306,  // Certifique-se de que a porta está configurada corretamente
 })
 
 try {
     sequelize.authenticate()
-    console.log('conectamos')
+    console.log('Conectamos ao banco de dados!')
 } catch (error) {
-    console.log(`não conectamos:${err}`)
+    console.log(`Não foi possível conectar ao banco de dados: ${error}`)
 }
 
 module.exports = sequelize
